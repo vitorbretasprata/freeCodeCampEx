48493fba350455a2abde9a2add0b8681b6f97016
Vitor <vitorbretasprata@gmail.com>
2019-02-21 20:00:45 -0300

    changes

--- Javascript/cashRegister.js
+++ Javascript/cashRegister.js
@@ -1,57 +1,65 @@
+/** In this challenge I've answered using the solution, soon I will use a different approach */
+
+var moneyTable = [
+    { name: 'ONE HUNDRED', val: 100.00},
+    { name: 'TWENTY', val: 20.00},
+    { name: 'TEN', val: 10.00},
+    { name: 'FIVE', val: 5.00},
+    { name: 'ONE', val: 1.00},
+    { name: 'QUARTER', val: 0.25},
+    { name: 'DIME', val: 0.10},
+    { name: 'NICKEL', val: 0.05},
+    { name: 'PENNY', val: 0.01}
+  ];
+
 function checkCashRegister(price, cash, caixa) {
-    var change = cash - price;
-    
+    var change = cash - price;    
     return payMoney(change, caixa);
 }
   
-  function payMoney(change, caixa){
-    let moneyArr = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]]
-     
-    let balance = change;    
-           
-    for(let i = moneyArr.length-1; i >= 0; i--){
-        if(moneyArr[i][1] < balance){
-            balance -= moneyArr[i][1];
+function payMoney(change, caixa){  
+    
+    let reg = caixa.reduce((accumutator, current) => {
+        accumutator.total += current[1];
+        accumutator[current[0]] = current[1]
 
-            if(moneyArr[i][1] > caixa[i][1] && i !== 0){
-                for(let j = i-1; j = 0; j--){
-                    console.log(moneyArr[j][1] + " - " + caixa[j][1])
-                    if(moneyArr[j][1] > caixa[j][1] && j === 0){
-                        return {status: "INSUFFICIENT_FUNDS", change: []};
-                    } else {
-                        caixa[j][1] = caixa[j][1] - moneyArr[j][1];
-                        console.log("3123")
-                        break;
-                    }
-                }
-            } 
-            
-            caixa[i][1] = caixa[i][1] - moneyArr[i][1];             
-            break;      
-        }    
-    }   
+        return accumutator;
+    }, { total: 0 });
 
-    if(balance > 0){
-        let x = payMoney(balance, caixa)
-        return x;
-    } else {
-        return {status: "OPEN", change: [...balance]}
+    
+    if(reg.total < change) {
+        return {status: "INSUFFICIENT_FUNDS", change: []};
     }
-  }
 
-  function loop(i, ){
+    if(reg.total === change) {
+        return {status: "CLOSED", change: caixa};
+    }  
+          
+    let arr = moneyTable.reduce((accumutator, current) => {
+        let value = 0;
 
-  }
+        while(reg[current.name] > 0 && change >= current.val){
+            change -= current.val;            
+            reg[current.name] -= current.val;
+            value += current.val;
+
+            change = Math.round(change * 100) / 100;
+        }
+        
+        
+        console.log(change)
+        if(value > 0) {
+            accumutator.push([current.name, value]);
+        }
+
+        return accumutator;
+    }, []);    
+    
+    if(arr.length < 1 || change > 0){
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+
+    return {status: "OPEN", change: arr};
+}
   
-  // Example cash-in-drawer array:
-  // [["PENNY", 1.01],
-  // ["NICKEL", 2.05],
-  // ["DIME", 3.1],
-  // ["QUARTER", 4.25],
-  // ["ONE", 90],
-  // ["FIVE", 55],
-  // ["TEN", 20],
-  // ["TWENTY", 60],
-  // ["ONE HUNDRED", 100]]
-  
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
